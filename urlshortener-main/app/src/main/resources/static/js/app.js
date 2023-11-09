7934b9c3a0c6ffe946be0afda006cd11857fca2e
Raul <795333@unizar.es>
2023-11-09 19:23:38 +0100

    html y app.js empezados -> QR

--- urlshortener-main/app/src/main/resources/static/js/app.js
+++ urlshortener-main/app/src/main/resources/static/js/app.js
@@ -3,17 +3,39 @@ $(document).ready(
         $("#shortener").submit(
             function (event) {
                 event.preventDefault();
+                let isQRChecked = $("#QRcheckbox").prop("checked");
                 $.ajax({
                     type: "POST",
                     url: "/api/link",
                     data: $(this).serialize(),
-                    success: function (msg, status, request) {
-                        $("#result").html(
-                            "<div class='alert alert-success lead'><a target='_blank' href='"
-                            + request.getResponseHeader('Location')
-                            + "'>"
-                            + request.getResponseHeader('Location')
-                            + "</a></div>");
+                    success: async function (msg, status, request) {
+
+                        if(isQRChecked){
+                            console.log("SIIIIIIIIIII SE HA PULSADO QR");
+                            
+                            $("#result").html(
+                                "<div class='alert alert-success lead'><a target='_blank' href='"
+                                + request.getResponseHeader('Location')
+                                + "'>"
+                                + request.getResponseHeader('Location')
+                                + "</a></div>"
+                                + "<div class='alert alert-success lead'><a target='_blank' href='"
+                                + request.getResponseHeader('Location') + "/qr"
+                                + "'>"
+                                + request.getResponseHeader('Location') + "/qr"
+                                + "</a></div>"
+                                );
+                        }else{
+                            console.log("NOOOOOOOOOOO SE HA PULSADO QR");
+
+                            $("#result").html(
+                                "<div class='alert alert-success lead'><a target='_blank' href='"
+                                + request.getResponseHeader('Location')
+                                + "'>"
+                                + request.getResponseHeader('Location')
+                                + "</a></div>");
+                        }
+                        
                     },
                     error: function () {
                         $("#result").html(
