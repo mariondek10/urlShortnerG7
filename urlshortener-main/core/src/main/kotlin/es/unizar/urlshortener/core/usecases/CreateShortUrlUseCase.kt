926f0c9217ec25bb7eed5318f0562f31ba8540b4
Jorge Sanclemente <jsanclemente@MacBook-Pro-de-Jorge.local>
2023-11-22 15:01:14 +0100

    Funcionalidad Uri alcanzable integrada con Qr. Funcionando Alcanzabilidad, falta verificar errores qr y testing sin hacer de ambas

--- urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlUseCase.kt
+++ urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlUseCase.kt
@@ -19,47 +19,55 @@ interface CreateShortUrlUseCase {
  */
 class CreateShortUrlUseCaseImpl(
     private val shortUrlRepository: ShortUrlRepositoryService,
+    private val isReachableUseCase: IsReachableUseCase,
     private val validatorService: ValidatorService,
     private val hashService: HashService
 ) : CreateShortUrlUseCase {
-    override fun create(url: String, data: ShortUrlProperties): ShortUrl =
-        shortUrlRepository.findByKey(hashService.hasUrl(url))?.let { shortUrl ->
-            if (shortUrl.properties.qrBool == false && data.qrBool == true) {
-                //no esta el qr(false) y se requiere (true)
-                val id: String = hashService.hasUrl(url)
-                val su = ShortUrl(
-                        hash = id,
-                        redirection = Redirection(target = url),
-                        properties = ShortUrlProperties(
+    override fun create(url: String, data: ShortUrlProperties): ShortUrl = when {
+            !validatorService.isValid(url) -> throw InvalidUrlException(url)
+            !isReachableUseCase.isReachable(url) -> throw UrlToShortNotReachable(url)
+            else -> {
+                shortUrlRepository.findByKey(hashService.hasUrl(url))?.let { shortUrl ->
+                    if (shortUrl.properties.qrBool == false && data.qrBool == true) {
+                        //no esta el qr(false) y se requiere (true)
+                        val id: String = hashService.hasUrl(url)
+                        val su = ShortUrl(
+                            hash = id,
+                            redirection = Redirection(target = url),
+                            properties = ShortUrlProperties(
                                 safe = shortUrl.properties.safe,
                                 ip = data.ip,
                                 sponsor = data.sponsor,
                                 qrBool = data.qrBool,
+                            )
                         )
-                )
-                shortUrlRepository.save(su)
-            } else {
-                shortUrl
-            }
-        }?: run{
-            if (validatorService.isValid(url)) {
-                System.out.println("(CreateShortUrlUseCase) data: ShortUrlProperties:" + data)
-                val id: String = hashService.hasUrl(url)
-                val su = ShortUrl(
-                        hash = id,
-                        redirection = Redirection(target = url),
-                        properties = ShortUrlProperties(
+                        shortUrlRepository.save(su)
+                    } else {
+                        shortUrl
+                    }
+                }?: run{
+                    if (validatorService.isValid(url)) {
+                        System.out.println("(CreateShortUrlUseCase) data: ShortUrlProperties:" + data)
+                        val id: String = hashService.hasUrl(url)
+                        val su = ShortUrl(
+                            hash = id,
+                            redirection = Redirection(target = url),
+                            properties = ShortUrlProperties(
                                 safe = data.safe,
                                 ip = data.ip,
                                 sponsor = data.sponsor,
                                 qrBool = data.qrBool
+                            )
                         )
-                )
-                System.out.println("(CreateShortUrlUseCase) antes de save su: ShortUrl:" + su)
-                shortUrlRepository.save(su)
-            } else {
-                throw InvalidUrlException(url)
+                        System.out.println("(CreateShortUrlUseCase) antes de save su: ShortUrl:" + su)
+                        shortUrlRepository.save(su)
+                    } else {
+                        throw InvalidUrlException(url)
+                    }
+                }
+
             }
         }
+
 
 }
