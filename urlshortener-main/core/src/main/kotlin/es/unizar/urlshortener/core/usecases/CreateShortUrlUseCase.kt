26b23cb130c5d738efa90f30d6301979ee5290a8
Marion de Kerchove <905027@unizar.es>
2023-12-13 17:37:43 +0100

    excepcion barra y comentarios

--- urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlUseCase.kt
+++ urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlUseCase.kt
@@ -32,6 +32,7 @@ class CreateShortUrlUseCaseImpl(
      * @return The generated ShortUrl object representing the shortened URL.
      * @throws InvalidUrlException If the provided URL is invalid.
      * @throws UrlToShortNotReachable If the URL to be shortened is not reachable.
+     *
      */
     override fun create(url: String, data: ShortUrlProperties): ShortUrl = when {
             !validatorService.isValid(url) -> throw InvalidUrlException(url)
@@ -56,7 +57,7 @@ class CreateShortUrlUseCaseImpl(
                         shortUrl
                     }
                 }?: run{
-                    if (validatorService.isValid(url)) {
+                    if (validatorService.isValid(url) && validatorService.withoutSlash(data.alias)) {
                         System.out.println("(CreateShortUrlUseCase) data: ShortUrlProperties:" + data)
                         val hash: String = data.alias ?: hashService.hasUrl(url)
                         val su = ShortUrl(
