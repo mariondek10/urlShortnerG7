31b332112dc9ca19f1432d7a6265c045624996be
Marion de Kerchove <905027@unizar.es>
2023-12-14 23:01:28 +0100

    rollback db blocking

--- urlshortener-main/repositories/src/main/kotlin/es/unizar/urlshortener/infrastructure/repositories/Repositories.kt
+++ urlshortener-main/repositories/src/main/kotlin/es/unizar/urlshortener/infrastructure/repositories/Repositories.kt
@@ -3,8 +3,8 @@ package es.unizar.urlshortener.infrastructure.repositories
 import es.unizar.urlshortener.core.Click
 import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Service
+//import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+//import org.springframework.stereotype.Service
 
 /**
  * Specification of the repository of [ShortUrlEntity].
@@ -24,7 +24,7 @@ interface ClickEntityRepository : JpaRepository<ClickEntity, Long> {
     fun findAllByHash(hash: String): List<ClickEntity>
 }
 
-interface UserRepository : CoroutineCrudRepository<SecurityProperties.User, Long> {
+/*interface UserRepository : CoroutineCrudRepository<SecurityProperties.User, Long> {
     override suspend fun findById(id: Long): SecurityProperties.User?
-}
+}*/
 
