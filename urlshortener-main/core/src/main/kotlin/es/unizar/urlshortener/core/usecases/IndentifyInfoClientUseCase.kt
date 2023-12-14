ab8d7424a20aa4c09536c8bf5345c4e5ba571363
jsanclemente <jsanclementev@gmail.com>
2023-12-14 18:30:01 +0100

    Funcionalidad de obtener informacion de la cabecera del cliente hecha. Comentado de la funcionalidad de isReachable de IdentifyUserAgentInfo con el marcado de kotlin

--- urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/IndentifyInfoClientUseCase.kt
+++ urlshortener-main/core/src/main/kotlin/es/unizar/urlshortener/core/usecases/IndentifyInfoClientUseCase.kt
@@ -8,18 +8,30 @@ import es.unizar.urlshortener.core.*
 /**
  *
  * Given an id representing a Short Url, returns accumulated User-Agent info.
- *
- *
  */
+
 interface IdentifyInfoClientUseCase {
+        /**
+         * Retrieves accumulated User-Agent info for a Short Url.
+         * @param id Short Url hash
+         * @return Map containing User-Agent info grouped by platform and browser with their respective counts
+         */
         fun returnInfoShortUrl(id: String): Map<String, Int>
 }
 
-
+/**
+ * Implementation of IdentifyInfoClientUseCase interface.
+ * @param clickRepository Service to interact with Click data
+ */
 class IdentifyInfoClientUseCaseImpl(
     private val clickRepository: ClickRepositoryService
 ) : IdentifyInfoClientUseCase {
 
+    /**
+     * Retrieves accumulated User-Agent info for a Short Url.
+     * @param id Short Url id
+     * @return Map containing User-Agent info grouped by platform and browser with their respective counts
+     */
     override fun returnInfoShortUrl(id: String): Map<String, Int> {
         val clicks = clickRepository.findByUrlHash(id)
         return clicks
